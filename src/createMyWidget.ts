c68d134ee68b45e6b6b76bc01f13c904fe1ea6e1
Tom Dye <github@tomdye.co.uk>
2016-12-07 17:55:56 +0000

    working with css var

--- src/createMyWidget.ts
+++ src/createMyWidget.ts
@@ -2,8 +2,8 @@ import { Widget, DNode, WidgetState } from 'dojo-widgets/interfaces';
 import createWidgetBase from 'dojo-widgets/createWidgetBase';
 import d from 'dojo-widgets/d';
 import * as css from './styles/my-widget.module.styl';
-import * as defaultTheme from './themes/default/my-widget.module.styl';
-import { assign } from 'dojo-core/lang';
+// import * as defaultTheme from './themes/default/my-widget.module.styl';
+// import { assign } from 'dojo-core/lang';
 
 export type MyWidgetTheme = {
 	root: string;
@@ -17,13 +17,13 @@ export type MyWidget = Widget<MyWidgetState>
 
 const createMyWidget = createWidgetBase.override({
 	getChildrenNodes(this: MyWidget): DNode[] {
-		const { theme:stateTheme = {} } = this.state;
-		const theme = assign(defaultTheme, stateTheme);
+		// const { theme:stateTheme = {} } = this.state;
+		// const theme = assign(defaultTheme, stateTheme);
 
 		return [
-			d(`div.${css.root}.${theme.root}`, [
+			d(`div.${css.root}`, [
 				'Hello,',
-				d(`span.${css.strong}.${theme.strong}`, [ 'Dojo' ]),
+				d(`span.${css.strong}`, [ 'Dojo' ]),
 				'World!'
 			])
 		];
--- src/main.ts
+++ src/main.ts
@@ -8,15 +8,15 @@ import createMyWidget from './createMyWidget';
 import createStore from 'dojo-stores/store/createStore';
 import createObservableStoreMixin from 'dojo-stores/store/mixins/createObservableStoreMixin';
 
-import * as blueMyWidgetTheme from './themes/blue/my-widget.module.styl';
-import * as greenMyWidgetTheme from './themes/green/my-widget.module.styl';
+import * as theme from './themes/blue/theme.module.styl';
+// import * as greenMyWidgetTheme from './themes/green/my-widget.module.styl';
 
 const createObservableStore = createStore.mixin(createObservableStoreMixin())
 const widgetStore = createObservableStore({
 	data: [
 		{
-			id: 'widget2',
-			theme: blueMyWidgetTheme
+			id: 'widget2'
+			// theme: blueMyWidgetTheme
 		}
 	]
 });
@@ -25,21 +25,24 @@ const projector = createProjector({
 	root: document.body
 });
 
-let blue = true;
-function changeTheme() {
-	let theme = blueMyWidgetTheme;
-	if (blue) {
-		theme = greenMyWidgetTheme;
-	}
-	blue = !blue;
-	widgetStore.patch({id: 'widget2', theme });
-}
+// let blue = true;
+// function changeTheme() {
+// 	let theme = blueMyWidgetTheme;
+// 	if (blue) {
+// 		theme = greenMyWidgetTheme;
+// 	}
+// 	blue = !blue;
+// 	widgetStore.patch({id: 'widget2', theme });
+// }
 
 projector.children = [
-	d(createMyWidget, { id: 'widget1' }),
-	d(createMyWidget, <WidgetOptions<WidgetState>> { id: 'widget2', stateFrom: widgetStore }),
-	d('button', {
-		'onclick': changeTheme
-	}, [ 'Change theme' ])
+	d(`div.${theme.theme}`, [
+		d(createMyWidget, { id: 'widget1' }),
+		d(createMyWidget, <WidgetOptions<WidgetState>> { id: 'widget2', stateFrom: widgetStore })
+	])
+
+	// d('button', {
+	// 	'onclick': changeTheme
+	// }, [ 'Change theme' ])
 ];
 projector.append();
