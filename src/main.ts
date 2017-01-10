d41beff828dbe8e9514b6daf7fe29af435b8f219
Tom Dye <github@tomdye.co.uk>
2017-01-10 11:40:51 +0000

    working with no stylus and updated webpack build

--- src/main.ts
+++ src/main.ts
@@ -2,17 +2,17 @@
 import createProjector from 'dojo-widgets/createProjector';
 import createWidgetBase from 'dojo-widgets/createWidgetBase';
 import { v, w } from 'dojo-widgets/d';
-import themeManager from 'dojo-widgets/themeManager';
+import themeManager, { Theme } from 'dojo-widgets/themeManager';
 import createTabPanel from 'dojo-widgets/components/tabpanel/createTabPanel';
 import createButton from 'dojo-widgets/components/button/createButton';
 import { Projector } from 'maquette';
 import { DNode, Widget, WidgetState, WidgetOptions } from 'dojo-widgets/interfaces';
 
-import * as dojoCommon from 'dojo-widgets/styles/common.module.styl';
-import * as noir from './themes/noir/theme.module.styl';
-import * as tabPanelOverrides from './styles/tabPanelOverrides.module.styl';
+import * as dojoCommon from 'dojo-widgets/styles/common';
+import * as noir from './themes/noir/theme.css';
+import * as tabPanelOverrides from './styles/tabPanelOverrides.css';
 
-themeManager.theme = noir;
+themeManager.theme = <Theme> <any> noir;
 
 const projector = createProjector({
 	root: document.body
@@ -22,6 +22,6 @@ projector.children = [
 	v('h2', ['Theme Manager']),
 	w(createTabPanel, { properties: { overrideClasses: tabPanelOverrides } }),
 	w(createButton, { properties: { label: 'Hello World' }}),
-	v(`h2.${dojoCommon.pullRight}`, ['im on the right'])
+	v(`h2`, ['im on the right'])
 ];
 projector.append();
---
+++ src/styles/tabPanelOverrides.css.d.ts
@@ -0,0 +1,1 @@
+export const tabPanelActiveTab: string;
