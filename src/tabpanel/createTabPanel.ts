828c9f0158bc7d6924b16282796b3b4c3876cd67
Tom Dye <github@tomdye.co.uk>
2017-01-19 14:04:55 +0000

    before adding

--- src/tabpanel/createTabPanel.ts
+++ src/tabpanel/createTabPanel.ts
@@ -2,20 +2,20 @@ import { ComposeFactory } from 'dojo-compose/compose';
 import { Widget, WidgetOptions, WidgetProperties, WidgetState, DNode } from 'dojo-widgets/interfaces';
 import createWidgetBase from 'dojo-widgets/createWidgetBase';
 import { v } from 'dojo-widgets/d';
-import * as css from './tabpanel.css';
 import themeable, { Themeable } from 'dojo-widgets/mixins/themeable';
+// import * as css from './tabpanel.css';
 
 export interface TabPanelState extends WidgetState {}
 
-export type TabPanel = Widget<WidgetProperties> & Themeable<typeof css>;
+export type TabPanel = Widget<WidgetProperties> & Themeable<any>;
 
 export interface TabPanelFactory extends ComposeFactory<TabPanel, WidgetProperties> {}
 
 const createTabPanel: TabPanelFactory = createWidgetBase.mixin(themeable).mixin({
 	mixin: {
 		tagName: 'tab-panel',
-		classes: [ css.tabPanel ],
-		baseTheme: css,
+		classes: [ 'css.tabPanel' ],
+		baseTheme: {},
 		getChildrenNodes: function (this: TabPanel): DNode[] {
 			return [
 				v(`ul`, { classes: this.theme.tabPanelTabs }, [
