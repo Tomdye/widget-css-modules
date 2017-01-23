0bf59eb933daa39b8740f7912d1d9fde11d929fb
Tom Dye <github@tomdye.co.uk>
2017-01-23 13:34:30 +0000

    latest widgets

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import createProjector from 'dojo-widgets/createProjector';
 import createWidgetBase from 'dojo-widgets/createWidgetBase';
 import { v, w } from 'dojo-widgets/d';
 import createTabPanel from './tabpanel/createTabPanel';
+import createButton from 'dojo-widgets/components/button/createButton';
 import { Projector } from 'maquette';
-import { DNode, Widget, WidgetState, WidgetOptions } from 'dojo-widgets/interfaces';
+import { DNode, Widget, WidgetOptions } from 'dojo-widgets/interfaces';
 
 // import * as dark from './themes/dark/theme.css';
 // import * as tabPanelOverrides from './styles/tabPanelOverrides.css';
@@ -15,12 +16,13 @@ const projector = createProjector({
 	root: document.body
 });
 
-projector.children = [
+projector.setChildren([
 	v('h1', ['Dojo 2 Demo']),
 	w(createTabPanel, {}),
+	w(createButton, { label: 'hello world' })
 	// w(createButton, { properties: { label: 'Hello World' } }),
 	// w(createTabPanel, { properties: { overrideClasses: tabPanelOverrides } }),
-];
+]);
 projector.append();
 
 // w(createTabPanel, { properties: { overrideClasses: tabPanelOverrides } }),
