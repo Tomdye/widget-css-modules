75cded123fdc6e6fac4c7f3a3a61a2baf7649a93
Tom Dye <github@tomdye.co.uk>
2017-02-17 18:05:38 +0000

    removed d.ts

--- src/tabpanel/styles/tabpanel.css.d.ts
+++
@@ -1,7 +0,0 @@
-export const root: string;
-export const tabs: string;
-export const tab: string;
-export const activeTab: string;
-export const panel: string;
-export const panels: string;
-export const apples: string;
